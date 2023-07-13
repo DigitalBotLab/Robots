7ad0e1185daaf887a8a67b8370e138fea7740363
yizhouzhao <yizhou_zhao@berkeley.edu>
2023-07-13 14:05:16 -0700

    update box mode

--- VisionServices/segment_anything/sam_app.py
+++ VisionServices/segment_anything/sam_app.py
@@ -22,6 +22,7 @@ def segment_with_points(
     image,
     input_point_x,
     input_point_y,
+    shape = "cuboid",
     input_label = np.array([1]),
     shape_contour_count = 6, 
     debug_plot = True,
@@ -35,38 +36,54 @@ def segment_with_points(
         multimask_output=True,
     )
 
+    print("mask", masks.shape, "scores", scores.shape, "logits", logits.shape)
+
     # only return the first mask
-    mask = masks[0].astype(np.uint8)
+    target_mask = masks[0].astype(np.uint8)
+    target_contours, _ = cv2.findContours(target_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    target_contour_count = len(target_contours)
 
-    # get contours
-    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    
-    # only return the first contour
-    contour = contours[0] 
+    for mask in masks:
+        # get contours
+        contours, _ = cv2.findContours(mask.astype(np.uint8), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        if len(contours) > 0 and len(contours[0]) < target_contour_count:
+            target_mask = mask
+            target_contours = contours
+            target_contour_count = len(contours)
+
+    if debug_plot:
+        cv2.drawContours(image, target_contours, -1, (255, 255, 255), 2)
+        cv2.imshow('target_contours', image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    mask = target_mask
+    contour = max(target_contours, key=cv2.contourArea)
     arclen = cv2.arcLength(contour, True)
-
-    for ratio in [0.005, 0.01, 0.02, 0.05, 0.1]:
-        epsilon = ratio * arclen
-        approx = cv2.approxPolyDP(contour, epsilon, True)
-        if len(approx) == shape_contour_count:
-            break
+    
+    if shape == "cuboid":
+        for ratio in [0.005, 0.01, 0.02, 0.05, 0.1]:
+            epsilon = ratio * arclen
+            approx = cv2.approxPolyDP(contour, epsilon, True)
+            if len(approx) == shape_contour_count:
+                break
+    else: # bounding box
+        x, y, w, h = cv2.boundingRect(contour)
+        approx = np.array([[[x, y]], [[x+w, y]], [[x+w, y+h]], [[x, y+h]]])
     
     print("approx", approx, approx.shape)
 
     if debug_plot:
-        temp = np.zeros((mask.shape[0], mask.shape[1], 1))
-        temp = cv2.drawContours(temp, [approx], -1, (255, 255, 255), 1)
-        color = np.array([0 / 255, 0 / 255, 255 / 255, 0.9])
-        contour_mask = temp / 255 * color.reshape(1, 1, -1)
+        temp = cv2.drawContours(image, [approx], -1, (255, 0, 0), 1)
 
         temp = cv2.resize(temp, (960, 540)) 
-        cv2.imshow('Blue Contours', temp)
+        cv2.imshow('Final Contours', temp)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
     return json.dumps(approx.tolist())
 
-cond_img_e = gr.Image(label="Input image", type='numpy')
+cond_img_e = gr.Image(label="Input image", type='numpy', image_mode = "RGB")
 input_point_x = gr.Number(label="input h", value = 0)
 input_point_y = gr.Number(label="input w", value = 0)
 
@@ -77,6 +94,7 @@ if __name__ == "__main__":
         inputs=[cond_img_e, 
                 input_point_x, 
                 input_point_y,
+                "text"
         ],
         outputs="text",
         title="Segment Anything",
